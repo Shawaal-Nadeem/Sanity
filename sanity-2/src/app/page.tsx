14717b4c525e2da8b90ee9bf17dc375d153f4b16
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-10-15 21:30:36 +0500

    image get from sanity

--- sanity-2/src/app/page.tsx
+++ sanity-2/src/app/page.tsx
@@ -1,9 +1,12 @@
+import { urlForImage } from "../../sanity/lib/image";
+import product from "../../sanity/product";
 import { client } from "./lib/sanityClient"
-
+import Image from "next/image";
 export const getProduct = async () => {
   const productData = await client.fetch(`*[_type=="product"]{
       title,
       description,
+      image
      }`)
   return productData;
    }
@@ -11,16 +14,18 @@ export const getProduct = async () => {
 export default async function Home() {
 
   let recvData = await getProduct();
+  console.log('Data Output')
   console.log(recvData);
    
   return (
     <>
-      {recvData.map((item: any) => {
+      {recvData.map((item: any,index:number) => {
         return (
-          <>
+          <div key={index}>
           <h1>{item.title}</h1>
           <h1>{item.description}</h1>
-          </>
+          <Image src={urlForImage(item.image.asset).url()} width={100} height={100} alt="Loading"></Image>
+         </div>
         )
       })}
     </>
